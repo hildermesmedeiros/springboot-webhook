f93743c5d83569f8c8ae5c35bfbf0ebf6e1cde25
Hildermes José Medeiros Filho <hildermes@gmail.com>
2024-04-29 11:27:03 -0300

    Create WebhookapiApplicationTests.java
    Todo, will do....not need for a POC

---
+++ webhookapi/src/test/java/com/imgwebhook/WebhookapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imgwebhook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
